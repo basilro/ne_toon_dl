3d3860b789e64245291575ac131bb57ef36b1cec
basilro <sica4311@gmail.com>
2026-06-10 11:50:08 +0900

    feat: 매뉴얼탭 관리 패널(원장/검증) + 다운로드 후처리 훅

--- __init__.py
+++ __init__.py
@@ -10,3 +10,8 @@ try:
     from curl_cffi import requests as _cffi  # noqa
 except Exception:
     os.system("pip install curl_cffi")
+
+try:
+    import nacl  # noqa
+except Exception:
+    os.system("pip install pynacl")
